cd774fb0f8c22d57e1d1e2d4639e3de873adb8f7
Brian Zhang <brianzhangw@gmail.com>
2018-07-25 17:57:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  var nested = document.getElementsByClassName('nested');
+  console.log(nested);
+  var target = nested.getElementById('target');
+  return target;
+}
