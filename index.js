41ad14abb43c39f6706fad4aa398e7893b52d05e
Brian Zhang <brianzhangw@gmail.com>
2018-07-25 18:09:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.querySelector('#nested').querySelector('.target');
+}
+
+function increaseRankBy(n){
+  var list = document.querySelectorAll('.ranked-list');
+  
+  for(let i = 0; i < list.length; i++){
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n;
+  }
+}
+
+function deepestChild(){
+  var list = document.querySelector('div#grand-node');
+ 
+  while(list.querySelector('div')){
+    list = list.querySelector('div');
+  }
+ 
+  return list
+}
